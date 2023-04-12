0ab01dec0ea9f4bf50fc29d7984d232d7cf3152f
gostavo21 <108429197+gostavo21@users.noreply.github.com>
2023-04-12 22:14:28 +0300

    Update GuessNumber.py

--- GuessNumber.py
+++ GuessNumber.py
@@ -6,18 +6,19 @@ computerNumber = random.randint(0,10)
 print("You Now Start The Guessing Number game \n Guess The Computer Number \n Good Luck ")
 # get a number from user
 userNumber = int(input("Enter a Number Between 1 and 10:"))
-#creat and give (a) a value
-a = 5
+
 #check if the user guess the computer number 
 if computerNumber == userNumber :
     #if the user guess the computer number 
     print("You Guessed!")
 #other way if the user didnt guess the computer number
 elif computerNumber != userNumber :
-        #make a loop 4 time
-        while a > 1 :
+	#creat and give (a) a value
+	a = 4
+        #make a loop 4 time                                                                                                           
+	while a > 0 :
             #tell the user the number of attempts rest
-            print("You Have " + str(a-1) + " Attempts") 
+            print("You Have " + str(a) + " Attempts") 
             #get a number from user again
             userNumber21 = int(input("Enter a Number Between 1 and 10:"))
             #check again if the user guess the computer number
@@ -25,8 +26,8 @@ elif computerNumber != userNumber :
                 #if the user guess the random number:
                 print("Yoo Hoo ,You Guessed!")
                 break
-            
+         
             a = a - 1
             
-            if a == 1 :
+            if a == 0 :
                 print("Sorry You  Run Out of Attempts.")
